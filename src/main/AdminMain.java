d760c33e51a3dcdce1fea2e1579eacf00e495cb7
Fabian996 <blauerdrache2@aol.de>
2015-10-13 20:20:04 +0200

    Update AdminMain.java

--- src/main/AdminMain.java
+++ src/main/AdminMain.java
@@ -12,37 +12,50 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import de.Fabian996.Admin.EventHandler.GhastTear;
-import de.Fabian996.Admin.Funktion.AdminFuncktion;
 
-public class AdminMain  extends JavaPlugin
+import de.Fabian996.Admin.EventHandler.BlazeRoad;
+import de.Fabian996.Admin.EventHandler.GhastTear;
+import de.Fabian996.Admin.Funktion.AdminFunction;
+import de.Fabian996.Admin.Funktion.ModeratorFunction;
+
+public class AdminMain extends JavaPlugin
 {
-	public Inventory inv = null;
 	
+	public Inventory inv = null;
+	public Inventory inv1 = null;
 	
 	public void onEnable()
 	{	
-		  System.out.println("[AdminInv] =================================");
+		System.out.println("[AdminInv] =================================");
 	    System.out.println("[AdminInv] Author: Fabian996");
 	    System.out.println("[AdminInv] Version: v" + getDescription().getVersion());
 	    System.out.println("[AdminInv] Status: Aktiviert");
 	    System.out.println("[AdminInv] =================================");
-		getServer().getPluginManager().registerEvents(new AdminFuncktion(), this);
-		getServer().getPluginManager().registerEvents(new GhastTear(), this);
+	    
+	    //Admin Inventory
+		getServer().getPluginManager().registerEvents(new AdminFunction(), this);
+
+		
+		//Moderator Inventory
+		getServer().getPluginManager().registerEvents(new ModeratorFunction(), this);
+
+		
+		//Menü Open
+		getServer().getPluginManager().registerEvents(new GhastTear(), this); // Admin
+		getServer().getPluginManager().registerEvents(new BlazeRoad(), this); // Moderator
 		
 	}
 	
 	
 	public void onDisable()
 	{
-      System.out.println("[AdminInv] =================================");
+		System.out.println("[AdminInv] =================================");
 	    System.out.println("[AdminInv] Author: Fabian996");
 	    System.out.println("[AdminInv] Version: v" + getDescription().getVersion());
 	    System.out.println("[AdminInv] Status: Deaktiviert");
 	    System.out.println("[AdminInv] =================================");
 	}
-	
-	
+
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{	Player p = (Player)sender;
@@ -50,12 +63,12 @@ public class AdminMain  extends JavaPlugin
 			if(p.hasPermission("admininv.inv")){
 				p.playSound(p.getLocation(), Sound.FIREWORK_BLAST, 1000.0F, 6.0F);
 				inv = p.getPlayer().getServer().createInventory(null, 45, ChatColor.RED + "Admin Inventar");
-				//				 |0 |1 |2 |3 |4 |5 |6 |7 |8 |
-				// 				 |9 |10|11|12|13|14|15|16|17|
-				// 				 |18|19|20|21|22|23|24|25|26|
-				// 				 |27|28|29|30|31|32|33|34|35|
-				//				 |36|37|38|39|40|41|42|43|44|
-				// Feautre: MonsterSpawn Inventory | Difficult	
+				//	|0 |1 |2 |3 |4 |5 |6 |7 |8 |
+				// 	|9 |10|11|12|13|14|15|16|17|
+				// 	|18|19|20|21|22|23|24|25|26|
+				// 	|27|28|29|30|31|32|33|34|35|
+				//	|36|37|38|39|40|41|42|43|44|
+				// Feautre: MonsterSpawn Inventory
 				
 				ItemStack Heal = new ItemStack(Material.POTION);
 				ItemMeta Healmeta = Heal.getItemMeta();
@@ -227,8 +240,166 @@ public class AdminMain  extends JavaPlugin
 				p.getPlayer().openInventory(inv);
 				
 				return true;
+				}
+			}else if(cmd.getName().equalsIgnoreCase("modinv")){
+				if(p.hasPermission("modinv.inv")){
+					p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1000.0F, 6.0F);
+					inv1 = p.getPlayer().getServer().createInventory(null, 45, ChatColor.DARK_GREEN +"Moderator Inventory");
+					//	|0 |1 |2 |3 |4 |5 |6 |7 |8 |
+					// 	|9 |10|11|12|13|14|15|16|17|
+					// 	|18|19|20|21|22|23|24|25|26|
+					// 	|27|28|29|30|31|32|33|34|35|
+					//	|36|37|38|39|40|41|42|43|44|
+					
+					ItemStack Heal = new ItemStack(Material.POTION);
+					ItemMeta Healmeta = Heal.getItemMeta();
+					ArrayList<String> Heal1 = new ArrayList<String>();
+					Healmeta.setDisplayName(ChatColor.DARK_RED + "Heal");
+					Heal1.add(ChatColor.WHITE + "You Heal by a Admin");
+					Healmeta.setLore(Heal1);
+					Heal.setItemMeta(Healmeta);
+					
+					ItemStack Fly = new ItemStack(Material.FEATHER);
+					ItemMeta Flymeta = Fly.getItemMeta();
+					ArrayList<String> Flyi = new ArrayList<String>();
+					Flymeta.setDisplayName(ChatColor.DARK_AQUA + "Fly <on/off>");
+					Flyi.add(ChatColor.DARK_AQUA + "You can Fly in the high");
+					Flymeta.setLore(Flyi);
+					Fly.setItemMeta(Flymeta);
+
+					ItemStack SM = new ItemStack(Material.BED);
+					ItemMeta SMmeta = SM.getItemMeta();
+					ArrayList<String> SuM = new ArrayList<String>();
+					SMmeta.setDisplayName(ChatColor.GOLD + "Survival Mode");
+					SuM.add(ChatColor.DARK_GREEN + "You can whit this Item you change your Mode to Survival");
+					SMmeta.setLore(SuM);
+					SM.setItemMeta(SMmeta);
+					
+					ItemStack CM = new ItemStack(Material.ENDER_PORTAL_FRAME);
+					ItemMeta CMmeta = CM.getItemMeta();
+					ArrayList<String> CrM = new ArrayList<String>();
+					CMmeta.setDisplayName(ChatColor.DARK_RED + "Creative Mode");
+					CrM.add(ChatColor.GOLD + "You can whit this Item you change your Mode to Creative");
+					CMmeta.setLore(CrM);
+					CM.setItemMeta(CMmeta);
+					
+					ItemStack AM = new ItemStack(Material.DISPENSER);
+					ItemMeta AMmeta = AM.getItemMeta();
+					ArrayList<String> AdM = new ArrayList<String>();
+					AMmeta.setDisplayName(ChatColor.DARK_AQUA + "Adventure Mode");
+					AdM.add(ChatColor.YELLOW + "You can whit this Item you change your Mode to Adventure");
+					AMmeta.setLore(AdM);
+					AM.setItemMeta(AMmeta);
+					
+					ItemStack Day = new ItemStack(Material.DAYLIGHT_DETECTOR);
+					ItemMeta Daymeta = Day.getItemMeta();
+					ArrayList<String> Sun = new ArrayList<String>();
+					Daymeta.setDisplayName(ChatColor.YELLOW + "Day");
+					Sun.add(ChatColor.GRAY + "You make Day of the World");
+					Daymeta.setLore(Sun);
+					Day.setItemMeta(Daymeta);
+					
+					ItemStack Night = new ItemStack(Material.NETHER_BRICK);
+					ItemMeta Nightmeta = Night.getItemMeta();
+					ArrayList<String> Nacht = new ArrayList<String>();
+					Nightmeta.setDisplayName(ChatColor.DARK_GRAY + "Night");
+					Nacht.add(ChatColor.GRAY + "You make Night of the World");
+					Nightmeta.setLore(Nacht);
+					Night.setItemMeta(Nightmeta);
+					
+					//Weather
+					ItemStack WeatherClear = new ItemStack(Material.WATER_BUCKET);
+					ItemMeta WeatherClearmeta= WeatherClear.getItemMeta();
+					ArrayList<String> Weather = new ArrayList<String>();
+					WeatherClearmeta.setDisplayName(ChatColor.GREEN + "Toggledownfall on");
+					Weather.add(ChatColor.DARK_PURPLE + "Weather on");
+					WeatherClearmeta.setLore(Weather);
+					WeatherClear.setItemMeta(WeatherClearmeta);
+					
+					ItemStack ClearWeather = new ItemStack(Material.BLAZE_POWDER);
+					ItemMeta ClearWeathermeta = ClearWeather.getItemMeta();
+					ArrayList<String> Weather1 = new ArrayList<String>();
+					ClearWeathermeta.setDisplayName(ChatColor.DARK_GREEN + "Toggledownfall off");
+					Weather1.add(ChatColor.DARK_PURPLE + "Weather off");
+					ClearWeathermeta.setLore(Weather1);
+					ClearWeather.setItemMeta(ClearWeathermeta);
+					
+					//Difficult <Peaceful/Easy/Normal/Hard>
+					ItemStack Peaceful = new ItemStack(Material.APPLE);
+					ItemMeta Peacefulmeta = Peaceful.getItemMeta();
+					ArrayList<String> P = new ArrayList<String>();
+					Peacefulmeta.setDisplayName(ChatColor.YELLOW + "Peaceful ");
+					P.add(ChatColor.AQUA + "Change to Peaceful Mode");
+					Peacefulmeta.setLore(P);
+					Peaceful.setItemMeta(Peacefulmeta);
+					
+					ItemStack Easy = new ItemStack(Material.ARROW);
+					ItemMeta Easymeta = Easy.getItemMeta();
+					ArrayList<String> E = new ArrayList<String>();
+					Easymeta.setDisplayName(ChatColor.GOLD + "Easy ");
+					E.add(ChatColor.AQUA + "Change to Easy Mode");
+					Easymeta.setLore(E);
+					Easy.setItemMeta(Easymeta);
+					
+					ItemStack Normal = new ItemStack(Material.ENCHANTMENT_TABLE);
+					ItemMeta Normalmeta = Normal.getItemMeta();
+					ArrayList<String> N = new ArrayList<String>();
+					Normalmeta.setDisplayName(ChatColor.GREEN + "Normal ");
+					N.add(ChatColor.AQUA + "Change to Normal Mode");
+					Normalmeta.setLore(N);
+					Normal.setItemMeta(Normalmeta);
+					
+					ItemStack Hard = new ItemStack(Material.TNT);
+					ItemMeta Hardmeta = Hard.getItemMeta();
+					ArrayList<String> H = new ArrayList<String>();
+					Hardmeta.setDisplayName(ChatColor.DARK_RED + "Hard ");
+					H.add(ChatColor.AQUA + "Change to Hard Mode");
+					Hardmeta.setLore(H);
+					Hard.setItemMeta(Hardmeta);
+					
+					//Vanish
+					ItemStack Vanish = new ItemStack(Material.SLIME_BALL);
+					ItemMeta Vanishmeta = Vanish.getItemMeta();
+					ArrayList<String> V = new  ArrayList<String>();
+					Vanishmeta.setDisplayName(ChatColor.DARK_PURPLE + "Vanish <on/off>");
+					V.add(ChatColor.DARK_AQUA + "You can change to Vanish and Spy the Player");
+					Vanishmeta.setLore(V);
+					Vanish.setItemMeta(Vanishmeta);
+					
+					
+					
+					//Inventory Line 1
+					inv1.setItem(0, Heal);
+					inv1.setItem(3, SM);
+					inv1.setItem(4, CM);
+					inv1.setItem(5, AM);
+					inv1.setItem(8, Fly);							
+					
+					//Inventory Line 2
+					
+					
+					//Inventory Line 3
+					inv1.setItem(18, Day);
+					inv1.setItem(19, Night);
+					inv1.setItem(23, Peaceful);
+					inv1.setItem(24, Easy);
+					inv1.setItem(25, Normal);
+					inv1.setItem(26, Hard);
+					
+					//Inventory Line 4
+					inv1.setItem(27, WeatherClear);
+					inv1.setItem(28, ClearWeather);
+					
+					//Inventory Line 5
+					
+					inv1.setItem(40, Vanish);
+					
+					
+					
+					p.getPlayer().openInventory(inv1);
+					return true;
+				}
 			}
-		}
 		return false;
 	}
-}	
+}
